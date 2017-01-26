c05b6f06048ed9b2792e9092fee43898bd8fd3aa
Daniel2218 <daniel_lucia22@hotmail.com>
2017-01-26 10:26:12 -0500

    Added some foundation code

---
+++ api.php
@@ -0,0 +1,105 @@
+<?php
+
+/**
+ * METHOD CLASS
+ * Seperates endpoints by request method and url. The idea is to provide simple
+ * syntax when defining endpoints. If the content type of the endpoints vary,
+ * this class should handle formatting and returning the desired content type.
+ */
+class Method {
+    public $request_method;
+    public $request_url;
+
+    public function __construct($request_method, $request_url) {
+        $request_method = strtoupper($request_method);
+
+        switch($request_method) {
+            case "GET": break;
+            case "POST": break;
+            case "DELETE": break;
+            case "PUT": break;
+            case "UPDATE": break;
+            // Invalid request method
+            default:
+                die("Method specified is invalid for endpoint " . $request_url);
+                break;
+        }
+
+        $this->request_method = $request_method;
+        $this->request_url    = $request_url;
+    }
+}
+
+/**
+ * ENDPOINT Class
+ * Stores all endpoints and provides simple syntax when declaring endpoints.
+ * Addition features would include error handling
+ */
+class Endpoint {
+    private static $endpoints = array();
+    public $action;
+    public $methods;
+
+    public function __construct($methods, $action) {
+        if (is_array($methods)) {
+            foreach ($methods as $method) {
+                if ($method instanceof Method == false) {
+                    die("Array methods does not have method types");
+                }
+            }
+        }
+        else {
+            die("parameter 1 is not an array");
+        }
+
+        $this->methods = $methods;
+        $this->action  = $action;
+
+        array_push(Endpoint::$endpoints, $this);
+    }
+
+    // TODO
+    // Find endpoint from list and call it
+    public static function call($request_method, $request_url) {
+        header("Content-Type: application/json");
+        foreach (Endpoint::$endpoints as $endpoint) {
+            foreach($endpoint->methods as $method) {
+                if($method->request_method == $request_method AND $method->request_url == $request_url) {
+                    $invoke = $endpoint->action;
+                    return $invoke();
+                }
+            }
+        }
+        die("Endpoint doed not exsist");
+    }
+}
+
+// Sample endpoint definitions
+new Endpoint(
+    array(
+        // new Method("GET", "/"),
+        new Method("POST", "example")
+    ),
+    function() {
+        $obj = array(
+            'Success'=>'true'
+        );
+        return json_encode($obj);
+    }
+);
+
+// new Endpoint(
+//     array(
+//         new Method("POST", "/"),
+//         new Method("UPDATE", "/door")
+//     ),
+//     function($args, $method) {
+//         return "{'door':'chair'}";
+//     }
+// );
+
+// Called on page load, passes request to handlers. ALL! information an Endpoint
+// should ever need must be passed through this function. It helps when
+// debugging!
+echo Endpoint::call($_SERVER['REQUEST_METHOD'], $_REQUEST['request']);
+?>
