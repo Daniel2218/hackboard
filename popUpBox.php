d9c4a144225d88303944bfc4fe85047805c972fd
Daniel2218 <daniel_lucia22@hotmail.com>
2017-03-01 19:51:17 -0500

    Delete file/small changes

--- popUpBox.php
+++ popUpBox.php
@@ -1,3 +1,19 @@
-<div id ="pop-up-box">
-    
 </div>
+</div>
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="css/popUpBox.css">
+    </head>
+        <body>
+            <div class="grandParentContaniner">
+                <div class="parentContainer">
+                    <h1> New User </h1>
+                    <input type="text" name="fname" placeholder="First Name"><br>
+                    <input type="text" name="lname" placeholder="Last Name"><br>
+                    <input type="text" name="email" placeholder="Email"><br>
+                    <input type="text" name="amount" placeholder="Amount"><br>
+                    <input type="submit" class ="hv-grow" value="Add">
+                </div>
+            </dvi>
+    </body>
+</html>
