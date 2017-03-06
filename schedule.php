940557b85b6dbc2409ee2e4ddac6f8cf70d21958
Daniel2218 <daniel_lucia22@hotmail.com>
2017-03-06 11:40:43 -0500

    Added drag and drop to schedule. Added small style changes

--- schedule.php
+++ schedule.php
@@ -32,15 +32,10 @@
 
                     <div id = "scheduleContainer">
                         <div id = "viewPanel">
-                            <div>
-                                <p> month </p>
-                            </div>
-                            <div>
-                                <p> week </p>
-                            </div>
-                            <div>
-                                <p> day </p>
-                            </div>
+                            <div id = "line"> </div>
+                            <div class = "month" id = "noBottomBorder" onclick="changeView('month')"> <p> month </p> </div>
+                            <div class = "week" onclick="changeView('week')"> <p> week </p> </div>
+                            <div class = "day" onclick="changeView('day')"> <p> day </p> </div>
                         </div>
                         <div id = "scheduleNav">
                             <i id = "i-left" class="fa fa-caret-left" aria-hidden="true"></i>
@@ -53,12 +48,12 @@
                                 <th>Mon</th>
                                 <th>Tue</th>
                                 <th>Wed</th>
-                                <th>Thur</th>
+                                <th>Thu</th>
                                 <th>Fri</th>
                                 <th>Sat</th>
                             </tr>
                             <tr>
-                                <td> 26</td>
+                                <td ondrop="drop(event)" ondragover = "allowDrop(event)"> 26</td>
                                 <td> 27</td>
                                 <td> 28</td>
                                 <td> 1</td>
@@ -74,6 +69,26 @@
                                 <td> 2</td>
                                 <td> 3</td>
                                 <td> 4</td>
+                            </tr>
+                        </table>
+                        <table id = "weekSced" style="width:100%;">
+                            <tr>
+                                <th>Sun 3/9</th>
+                                <th>Mon 3/10</th>
+                                <th>Tue 3/11</th>
+                                <th>Wed 3/12</th>
+                                <th>Thu 3/13</th>
+                                <th>Fri 3/14</th>
+                                <th>Sat 3/15</th>
+                            </tr>
+                            <tr>
+                                <td> </td>
+                                <td> </td>
+                                <td> </td>
+                                <td> </td>
+                                <td> </td>
+                                <td> </td>
+                                <td> </td>
                             </tr>
                         </table>
                     </div>
@@ -95,7 +110,45 @@
             div.style.marginBottom = "5px";
             div.style.fontFamily = "Open Sans, sans-serif";
             div.draggable = "true";
+            div.addEventListener('dragstart', function() {drag(event)}, false);
+            div.id = "drag1";
             document.getElementById("eventContainer").appendChild(div);
+        }
+
+        function allowDrop(ev) {
+            ev.preventDefault();
+        }
+
+        function drag(ev) {
+            ev.dataTransfer.setData("text/plain", ev.target.id);
+        }
+
+        function drop(ev) {
+            ev.preventDefault();
+            var data = ev.dataTransfer.getData("text");
+            var element = document.getElementById(data);
+            element.style.background = "#fb7a2c";
+            element.style.fontSize = "12px";
+            element.style.display = "block";
+            element.style.width = "95%";
+            element.style.padding = "6px 4px";
+            element.style.marginTop = "2px";
+            element.style.marginLeft = "0";
+            element.style.marginBottom = "0";
+            element.style.textAlign = "left";
+            element.style.fontWeight = "normal";
+            ev.target.appendChild(document.getElementById(data));
+        }
+
+        function changeView(viewType) {
+            if (viewType === "week") {
+                var current = document.getElementById("noBottomBorder");
+                var next = document.getElementsByClassName("week");
+                current.removeAttribute("noBottomBorder");
+                current.id = "";
+                next.id = "noBottomBorder"; // this doesnt work 
+                console.log("hello");
+            }
         }
     </script>
 </html>
